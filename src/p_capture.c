05d72680258f92953469dae838fe8cd7ad280f31
palloc <palloc.mknet10@gmail.com>
2017-01-16 12:25:58 +0900

    add struct

--- src/p_capture.c
+++ src/p_capture.c
@@ -11,17 +11,46 @@
 
 typedef struct {
 	int duration;
-	int ip_proto;
+	int protocol_type;
 	int service;
+	int flag;
 	int src_bytes;
 	int dst_bytes;
-	int flag;
 	int land;
 	int wrong_fragment;
 	int urgent;
-
 	int hot;
 	int num_failed_logins;
+	int logged_in;
+	int num_compromised;
+	int root_shell;
+	int su_attempted;
+	int num_root;
+	int num_file_creations;
+	int num_shells;
+	int num_access_files;
+	int num_outbound_cmds;
+	int is_host_login;
+	int is_guest_login;
+	int count;
+	int srv_count;
+	int serror_rate;
+	int srv_serror_rate;
+	int rerror_rate;
+	int srv_rerror_rate;
+	int same_srv_rate;
+	int diff_srv_rate;
+	int srv_diff_host_rate;
+	int dst_host_count;
+	int dst_host_srv_count;
+	int dst_host_same_srv_rate;
+	int dst_host_diff_srv_rate;
+	int dst_host_same_src_port_rate;
+	int dst_host_srv_diff_host_rate;
+	int dst_host_serror_rate;
+	int dst_host_srv_serror_rate;
+	int dst_host_rerror_rate;
+	int dst_host_srv_rerror_rate;
 } a_packet;
 
 int main()
